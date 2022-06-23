c3aaf01469f0461c719d0a47bc9e9d568e80f395
Scott <45835546+Reginld1408@users.noreply.github.com>
2022-06-22 21:08:31 -0600

    Update data-proxy.js

--- routes/data-proxy.js
+++ routes/data-proxy.js
@@ -1,18 +1,10 @@
 
 var httpProxy = require("http-proxy");
-
 var apiProxy = httpProxy.createProxyServer();
-
 var serverOne = "http://localhost:8050";
 
-
-
 module.exports = function (req, res) {
-
   if (!req.user || req.user.role != "manager") res.status(403).redirect("/");
-
-  console.log("redirecting to Server1");
-
-  apiProxy.web(req, res, { target: serverOne });
-
+   console.log("redirecting to Server1");
+   apiProxy.web(req, res, { target: serverOne });
 };
