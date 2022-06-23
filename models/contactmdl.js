92f08c73f7c48475a9301b9112d12d8b9d0a0a6b
Scott <45835546+Reginld1408@users.noreply.github.com>
2022-06-22 21:10:19 -0600

    Update contactmdl.js

--- models/contactmdl.js
+++ models/contactmdl.js
@@ -1,7 +1,6 @@
 // Require the mongoose module
 var mongoose = require("mongoose");
 // Set up a mongoose connection
-// var mongoDBurl = "mongodb://localhost:27017/contacts";
 
 const uniqueValidator = require("mongoose-unique-validator")
 const postSchema = new mongoose.Schema({
@@ -15,37 +14,15 @@ const postSchema = new mongoose.Schema({
       type: String,
       required: "Please write your post body.",
       trim: true,
-      // validate: {
-      //   validator: function (v) {
-      //     return v.length > 10;
-      //   },
-      //   message: (props) => `${props.value} is body is too short.`,
-      // },
     },
     phone: {
       type: String,
       trim: true,
-      // validate: {
-      //   validator: function (v) {
-      //     return /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(v);
-      //   },
-      //   message: (props) => `${props.value} is not a valid URL slug.`,
-      // },
-      // required: "Please enter the post url slug",
      },
     comment: {
       type: String,
       trim: true,
-      // validate: {
-      //   validator: function (v) {
-      //     return /^[a-z0-9]+(?:-[a-z0-9]+)*$/.test(v);
-      //   },
-      //   message: (props) => `${props.value} is not a valid URL slug.`,
-      // },
-      // required: "Please enter the post url slug",
     },
-    // user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    
     // more fields defined below
 });
 postSchema.plugin(uniqueValidator);
